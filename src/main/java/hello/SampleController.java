341c2f00ecbd0970ac1deb0fecc4e4acdeb836c9
Mason <mason@localhost.localdomain>
2018-09-28 15:09:17 +0900

    something was changed

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -20,6 +20,6 @@ public class SampleController {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
-        System.out.println("hihi");
+        System.out.println("1123");
     }
 }
